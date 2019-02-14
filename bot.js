86331ee344ffa25901931190908f82bbf1f278b3
brooksevan <ecb5723@truman.edu>
2019-02-14 14:56:28 -0600

    about to test first command. just a simple sending of a message

--- bot.js
+++ bot.js
@@ -3,25 +3,58 @@ const eris = require('eris');
 const PREFIX = '!';
 var auth = require('./auth.json');
 // Initialize Discord Bot
-const commandHandlerForCommandName[]
+const commandHandlerForCommandName = {};
 var bot = new eris.Client('NTQ0MzAzMTcwNTMzNDU3OTUz.D0ddGA.bRe_dJ0lvwpTBRKs0UsmLs7xYUQ');
+
+// handles a test command by saying hi
+commandHandlerForCommandName['sayhi'] => {
+    return msg.channel.createMessage('hi');
+};
+
+bot.on('messageCreate', async (msg) => {
+    const content = msg.content;
+    
+    //ignore direct messages
+    if (!msg.channel.guild) 
+    {
+        return;
+    }
+    
+    //ignore messages without prefix
+    if (!content.startsWith(PREFIX))
+    {
+        return;
+    }
+    
+    //get command
+    const parts - content.split(' ').map(s => s.trim()).filter(s => s);
+    const commandName = parts[0].substr(PREFIX.length)
+    
+    //get handler
+    cosnt commandHandler = commandHandlerForCommandName[commandName];
+    if (!commandHandler)
+    {
+        return msg.channel.createMessage('invalid command');
+    }
+    
+    try 
+    {
+        await commandHandler();
+    } 
+    catch (err)
+    {
+        console.warn('error handling command');
+        console.warn(err);
+    }
+});
+
+
+
 
 bot.on('ready', function (evt) {
     console.log('connected');
 });
-bot.on('messageCreate', async(msg) => {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    const botWasMentioned = msg.mentions.find(mentionedUser => mentionedUser.id === bot.user.id);
-    if (botWasMentioned) {
-        try {
-            await msg.channel.createMessage('here');
-        } catch (err) {
-            console.warn('failed');
-            console.warn(err);
-        }
-    }
-});
+
 
 
 
